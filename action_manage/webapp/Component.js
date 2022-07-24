b8846dc2cbd700ac8d78e890295d5ab2cd51071f
sangyoun kim <demitry1@gmail.com>
2022-07-24 23:35:34 +0900

    three

---
+++ action_manage/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("actionmanage.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
